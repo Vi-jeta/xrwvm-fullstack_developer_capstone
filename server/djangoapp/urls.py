dc9eac7979e5b66d3677bf5a7c3ce08e5a23ab7d
Vi-jeta <vijetasharma455@gmail.com>
2026-06-18 11:58:07 -0400

    car make

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
 
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='register', view=views.register, name='register'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
     # path for login
     # path(route='login', view=views.login_user, name='login'),
 
